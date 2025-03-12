1c3c022c4a18040e4579699d123e45eb6959ab0c
Md. Mostafijur Rahman <mrmoon025@gmail.com>
2025-03-12 19:23:57 +0600

    Time: 730 ms (87.86%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -2,4 +2,4 @@
 
 #select * from Customer as c join Product as p on c.product_key = p.product_key;
 
-select customer_id from Customer as c group by customer_id having count(distinct product_key) = (select count(product_key) from Product);
+select customer_id from Customer group by customer_id having count(distinct product_key) = (select count(product_key) from Product);
