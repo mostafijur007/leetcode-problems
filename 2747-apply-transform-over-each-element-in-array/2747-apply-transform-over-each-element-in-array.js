67bde1234b7db7abec8fea005dc96c10d41a4820
Md. Mostafijur Rahman <mrmoon025@gmail.com>
2025-03-15 21:58:32 +0600

    Time: 44 ms (71.76%), Space: 53.4 MB (24.76%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    // let transform = [];
+    let transform = [];
     for(let i = 0; i < arr.length; i++){
-        // transform[i] = fn(arr[i], i)
-         arr[i] = fn(arr[i], i);
+        transform[i] = fn(arr[i], i)
+        //  arr[i] = fn(arr[i], i);
     }
 
-    // return transform;
-    return arr;
+    return transform;
+    // return arr;
 };
