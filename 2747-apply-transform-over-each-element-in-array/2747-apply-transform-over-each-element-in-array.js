d1258f7d314daed9f037916e9cb948620c255c32
Md. Mostafijur Rahman <mrmoon025@gmail.com>
2025-03-15 21:55:31 +0600

    Time: 38 ms (90.52%), Space: 53.4 MB (27.8%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let transform = [];
+    for(let i = 0; i < arr.length; i++){
+        transform[i] = fn(arr[i], i)
+    }
+
+    return transform;
+};
